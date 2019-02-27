1f8f47c85c764d0f740d0b20ef6ff97b45bd32b0
github username <github email address>
2019-02-27 10:49:50 -0500

    Someone explain to me what 'helper method' is...

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,19 +1,26 @@
 import React from "react";
 import BotCard from "../components/BotCard";
 
-class BotCollection extends React.Component {
-  //your code here
 
-  render(){
-  	return (
-  	  <div className="ui four column grid">
-    		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
-    		</div>
-  	  </div>
-  	);
-  }
+class BotCollection extends React.Component {
+
+    showMyDamnBotsDammit = (props) => {
+        return bots.map(bots => {
+            return <BotCard bot={bot} />
+        })
+    }
+
+    render(){
+        console.log("Where the fuck are my bots?", this.state)
+        console.log("Where the fuck are my bots?", this.bot)
+        return (
+            <div className="ui four column grid">
+                <div className="row">
+                    {this.showMyDamnBotsDammit()}
+                </div>
+            </div>
+        );
+    }
 
 };
 
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,16 +1,38 @@
 import React from "react";
+import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
+
+const API = "https://bot-battler-api.herokuapp.com/api/v1/bots"
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
 
-  render() {
-    return (
-      <div>
-        {/* put your components here */}
-      </div>
-    );
-  }
+    state = {
+        bots: []
+    }
 
+    componentDidMount() {
+        fetch(API)
+        .then(res => res.json())
+        .then(res => {
+            this.setState({
+                bots: res
+            })
+        })
+    }
+
+    botProps = () => {
+        return this.state
+    }
+
+    render() {
+        console.log("These are my bots", this.state)
+        return (
+            <div>
+                <YourBotArmy />
+                <BotCollection bots={this.botProps()} />
+            </div>
+        );
+    }
 }
 
 export default BotsPage;
