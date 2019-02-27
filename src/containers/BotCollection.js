cf28d9029eeb018ab4736093cc20b6a1c5918f91
github username <github email address>
2019-02-27 11:09:56 -0500

    checkpoint

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -4,11 +4,21 @@ import BotCard from "../components/BotCard";
 class BotCollection extends React.Component {
   //your code here
 
+  makeCards = (bots) => {
+    return bots.map(bot => {
+      return <BotCard bot={bot} enlist={this.props.enlist}/>
+    })
+  }
+
+
   render(){
+
+    let botCards = this.makeCards(this.props.botList)
+
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
+    		  {botCards}
     		  Collection of all bots
     		</div>
   	  </div>
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,14 +1,75 @@
 import React from "react";
+import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
+import BotCard from "../components/BotCard"
 
 class BotsPage extends React.Component {
   //start here with your code for step one
 
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      botList: [],
+      enlisted: []
+    }
+  }
+
+
+  makeCards = (bots) => {
+    return bots.map(bot => {
+      return <BotCard bot={bot} enlist={this.enlist}/>
+    })
+  }
+
+
+  enlist = (bot) => {
+    // console.log(bot)
+    // console.log(this.state.enlisted)
+    if (this.state.enlisted.includes(bot)) return
+    this.setState({
+      enlisted: this.state.enlisted.concat(bot)
+    })
+  }
+
+  unenlist = (bot) => {
+    // const index = this.state.enlisted.indexOf(bot)
+    // let newArmy = []
+    // if (index > -1) {
+    //   newArmy = this.state.enlisted.splice(index, 1);
+    // }
+    let newArmy
+    newArmy = this.state.enlisted.filter((army) => {
+      if (army !== bot) {
+        return army
+      }
+    })
+
+    this.setState({
+      enlisted: newArmy
+    })
+  }
+
   render() {
     return (
       <div>
+        <YourBotArmy enlisted={this.state.enlisted} enlist={this.unenlist}/>
+        <BotCollection botList={this.state.botList} enlist={this.enlist}/>
         {/* put your components here */}
       </div>
     );
+  }
+
+
+  componentDidMount() {
+    let API = 'https://bot-battler-api.herokuapp.com/api/v1/bots'
+    fetch(API).then(res => res.json()).then(bots => {
+      console.log(bots)
+      this.setState({
+        botList: bots
+      })
+    })
   }
 
 }
