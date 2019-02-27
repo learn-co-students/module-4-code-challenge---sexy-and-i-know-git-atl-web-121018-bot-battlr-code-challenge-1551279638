0602939a1906b451e2cdf84ddaca89165031fe16
Cory <coadams9@gmail.com>
2019-02-27 11:31:45 -0500

    cory

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,31 @@
 import React from "react";
+import BotCollection from 'react';
+import YourBotArmy from 'react';
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+  constructor(props){
+    super(props)
+    this.state = {
+      botColl: this.props.bots,
+      botArmy: []
+    }
+  }
+
+  handleClick = (bot) => {
+    this.setState({
+      botArmy: [...this.state.botArmy, bot]
+    })
+  }
+
+
+
 
   render() {
+    console.log(this.props)
     return (
       <div>
-        {/* put your components here */}
+        {<BotCollection botColl={this.state.botColl} handleClick={this.handleClick}/>}
+        {<YourBotArmy botArmy={this.state.botArmy} />}
       </div>
     );
   }
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -1,22 +1,26 @@
 import React from "react";
 import BotCard from "../components/BotCard";
+import BotSpecs from '..components/BotSpecs'
 
 class YourBotArmy extends React.Component {
-  //your bot army code here...
+
+  botArmy = () => this.props.botArmy.map(bot => {
+    return <BotCard key={bot.id} bot={bot} />
+  })
 
   render(){
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
+            {this.botArmy()}
             Your Bot Army
           </div>
         </div>
       </div>
     );
   }
-  
+
 };
 
 export default YourBotArmy;
